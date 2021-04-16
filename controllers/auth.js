c59177a87f77bce682d4f2cfdcfd4e520157a888
LeoReyesParedes <leonardo.reyes.paredes@gmail.com>
2021-04-15 19:55:07 -0500

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -44,6 +45,49 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req = request, res = response) => {
+    
+    const {id_token} = req.body;
+
+    try {
+        const {name, mail, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({mail});
+
+        if(!usuario){
+            // Crear user
+            const data = {
+                name,
+                mail,
+                password: ':D',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+        // Usuario status false
+        if(!usuario.status){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado.'
+            });
+        }
+        // Generar JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de google no es válido.'
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- models/usuario.js
+++ models/usuario.js
@@ -20,6 +20,7 @@ const UsuarioSchema = Schema({
     role: {
         type: String,
         required: [true, 'El rol es obligatorio'],
+        default: 'USER_ROLE',
         enum: ['ADMIN_ROLE', 'USER_ROLE']
     },
     status: {
